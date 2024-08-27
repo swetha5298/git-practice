8ec396b1eeeaf393350207a9f8295f590605761f
gowniswetha <gowniswetha05@gmail.com>
2024-08-27 19:00:17 +0530

    i added a conversation

---
+++ 02.conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: how are you suresh?"
+echo "suresh:: hello ramesh, i am fine.whaat are you doing?"
+echo "ramesh:: i am doing good suresh,whats going on?"
+echo "suresh:: i started learning shell script ramesh"
