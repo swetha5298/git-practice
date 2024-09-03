d9779d883ff1a6f05978b351b61acd4a312ceb2b
gowniswetha <gowniswetha05@gmail.com>
2024-09-03 15:19:14 +0530

    addedconversation

--- 02.conversation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- PERSON1=raheem
- PERSON2=allah
-echo "${PERSON1}:: how are you ${PERSON2}?"
-echo "${PERSON2}:: hello ${PERSON1}, i am fine.whaat are you doing?"
-echo "${PERSON1}:: i am doing good ${PERSON2},whats going on?"
-echo "${PERSON2}:: i started learning shell script ${PERSON1}"
