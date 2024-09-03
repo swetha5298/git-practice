15ca70cd70b4e32085cd4aff3f2c3abf93cab839
gowniswetha <gowniswetha05@gmail.com>
2024-09-03 15:23:35 +0530

    namechange

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "suresh:: how are you ramesh?"
+echo "ramesh:: hello suresh, i am fine.whaat are you doing?"
+echo "suresh:: i am doing good ramesh,whats going on?"
+echo "ramesh:: i started learning shell script suresh"
