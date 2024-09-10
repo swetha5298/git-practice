6c01db2eec871d57a89a65f58f8c146db1589de4
gowniswetha <gowniswetha05@gmail.com>
2024-09-10 17:44:15 +0530

    variables

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
-for i in {0..100}
-do 
-  echo $i
-done
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+CHECK_ROOT (){
+    if [ $USERID -ne 0 ]
+then
+    echo "please run the script with root priveleges"
+    exit 1 
+     fi
+}
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+     echo -e "$2 is $R failed..$N"
+    exit 1
+    else
+    echo -e  "$2 is $G sucess..$N"
+    fi
+}
+
+CHECK_ROOT
+
+ for package in $@
+ do
+ echo $package
+ done
