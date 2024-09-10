d8eacc7d727499662242c71b492f7b5dbc4d6825
gowniswetha <gowniswetha05@gmail.com>
2024-09-10 15:16:07 +0530

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -5,7 +5,8 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-  echo "please run the script with root priveleges" 
+  echo "please run the script with root priveleges"
+  exit 1 
 fi
 
 dnf install git -y
