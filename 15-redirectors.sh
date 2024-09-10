6c2e354a88e087260f3d93f1b8b30ce3f5c9c215
gowniswetha <gowniswetha05@gmail.com>
2024-09-10 18:45:54 +0530

    variables

--- 15-redirectors.sh
+++ 15-redirectors.sh
@@ -43,7 +43,7 @@ fi
 for package in $@
  do
   dnf list install $package &>>$LOG_FILE
-if [$? -ne o ]
+if [ $? -ne 0 ]
 then
     echo "$package is not installed,going to install it .."&>>$LOG_FILE
     dnf install $package -y &>>$LOG_FILE
