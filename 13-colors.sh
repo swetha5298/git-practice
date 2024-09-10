f9fc5d98c7767bf09b75f1f3d9e975524d260f44
gowniswetha <gowniswetha05@gmail.com>
2024-09-10 16:58:00 +0530

    variables

---
+++ 13-colors.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+CHECK_ROOT (){
+    if [ $USERID -ne 0 ]
+then
+    echo "please run the script with root priveleges"
+    exit 1 
+     fi
+}
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+     echo -e "$2 is $R failed..$N"
+    exit 1
+    else
+    echo -e  "$2 is $G sucess..$N"
+    fi
+}
+
+CHECK_ROOT
+
+  dnf list install git
+
+VALIDATE $? "listing Git"
+if [ $? -ne 0 ]
+then 
+  echo "Git is not installed/going to install it.."
+  dnf install git -y
+VALIDATE $? "Installing Git "
+else
+    echo "Git is already installed,nothing to do .."
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+  echo "mysql is not installed/going to install it.."
+  dnf install mysql -y
+VALIDATE $?"installing mysql"
+else
+    echo "mysql is already installed,nothing to do .."
+fi
