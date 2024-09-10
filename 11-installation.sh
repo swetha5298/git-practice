6d16c79c9fdabc56b604ce0f30b0f8ca2f89566a
gowniswetha <gowniswetha05@gmail.com>
2024-09-10 15:21:23 +0530

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -9,4 +9,12 @@ then
   exit 1 
 fi
 
+dnf install git 
+
+if [ $? -ne 0 ]
+then 
+echo "Git is not installed/going to intsall it.."
 dnf install git -y
+else
+echo "Git is already installed,nothing to do .."
+fi
